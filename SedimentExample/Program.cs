7f86c3aa011a5b3784523229bec395b2f06c8d8f
DvdKhl <dvdkhl@gmail.com>
2014-07-29 00:16:57 +0200

    Updated Example

--- SedimentExample/Program.cs
+++ SedimentExample/Program.cs
@@ -2,6 +2,7 @@
 using Sediment.Core;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,45 @@ namespace SedimentExample {
 		static void Main(string[] args) {
 			var level = Level.Load(@"C:\Users\Arokh\AppData\Roaming\.minecraft\saves\Test");
 			var world = level.WorldManager[WorldInfo.Overworld];
+
+			var sw = new Stopwatch();
+
+			sw.Restart();
+			var blockMan = world.BlockManager;
+			for(int i = 0; i < 100; i++) {
+				for(int y = 0; y < Chunk.BlockYCount; y++) {
+					for(int x = 0; x < Chunk.BlockXCount; x++) {
+						for(int z = 0; z < Chunk.BlockZCount; z++) {
+							blockMan[x, y, z] = 1;
+						}
+					}
+				}
+			}
+			Console.WriteLine(sw.ElapsedMilliseconds);
+
 			var chunk = world.ChunkManager[0, 0];
-			chunk.UpdateHeightMap();
-			chunk.UpdateLighting();
+
+			sw.Restart();
+			for(int i = 0; i < 100; i++) {
+				for(int y = 0; y < Chunk.BlockYCount; y++) {
+					for(int x = 0; x < Chunk.BlockXCount; x++) {
+						for(int z = 0; z < Chunk.BlockZCount; z++) {
+							chunk[x, y, z] = 1;
+						}
+					}
+				}
+			}
+			Console.WriteLine(sw.ElapsedMilliseconds);
+
+			sw.Restart();
+			for(int i = 0; i < 100; i++) {
+				for(int j = 0; j < Chunk.BlockCount; j++) {
+					chunk[j] = 1;
+				}
+			}
+			Console.WriteLine(sw.ElapsedMilliseconds);
+
+			Console.Read();
 		}
 	}
 }
