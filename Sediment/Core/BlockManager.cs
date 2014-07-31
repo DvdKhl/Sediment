e5085d2dbd3191bc49ade9ebef2f67d0277defa5
DvdKhl <dvdkhl@gmail.com>
2014-07-31 23:25:29 +0200

    Big speedup in BlockManager for getting and setting blocks, if consecutive blocks are within the same chunk

--- Sediment/Core/BlockManager.cs
+++ Sediment/Core/BlockManager.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,14 +15,22 @@ namespace Sediment.Core {
 		}
 
 		public ushort this[int x, int y, int z] {
-			get {
-				var chunk = world.ChunkManager[x / Chunk.BlockXCount, z / Chunk.BlockZCount];
-				return chunk[x & Chunk.XMask, y, z & Chunk.ZMask];
-			}
-			set {
-				var chunk = world.ChunkManager[x / Chunk.BlockXCount, z / Chunk.BlockZCount];
-				chunk[x & Chunk.XMask, y, z & Chunk.ZMask] = value;
-			}
+			get { return GetChunk(x, z)[x & Chunk.XMask, y, z & Chunk.ZMask]; }
+			set { GetChunk(x, z)[x & Chunk.XMask, y, z & Chunk.ZMask] = value; }
 		}
+		
+		
+		private Chunk chunkCache;
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private Chunk GetChunk(int x, int z) {
+			var chunkX = x / Chunk.BlockXCount;
+			var chunkZ = z / Chunk.BlockZCount;
+		
+			if(chunkCache == null || chunkCache.X != chunkX || chunkCache.Z != chunkZ) {
+				chunkCache = world.ChunkManager[chunkX, chunkZ];
+			}
+			return chunkCache;
+		}
+
 	}
 }
