f1579d95e339fb6c0248f8259bd99b8328f53ff1
DvdKhl <DvdKhl@gmail.com>
2014-11-29 18:54:01 +0100

    Fixed Chunk retrieval; Added heightmap access

--- Sediment/Core/BlockManager.cs
+++ Sediment/Core/BlockManager.cs
@@ -15,22 +15,39 @@ namespace Sediment.Core {
 		}
 
 		public ushort this[int x, int y, int z] {
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
 			get { return GetChunk(x, z)[x & Chunk.XMask, y, z & Chunk.ZMask]; }
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
 			set { GetChunk(x, z)[x & Chunk.XMask, y, z & Chunk.ZMask] = value; }
 		}
-		
-		
+
+
 		private Chunk chunkCache;
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		private Chunk GetChunk(int x, int z) {
-			var chunkX = x / Chunk.BlockXCount;
-			var chunkZ = z / Chunk.BlockZCount;
-		
+			var chunkX = x >> Chunk.XBits;
+			var chunkZ = z >> Chunk.ZBits;
+
 			if(chunkCache == null || chunkCache.X != chunkX || chunkCache.Z != chunkZ) {
 				chunkCache = world.ChunkManager[chunkX, chunkZ];
 			}
 			return chunkCache;
 		}
 
+
+		public class HeightMapIndexer {
+			private BlockManager blockManager;
+
+			public HeightMapIndexer(BlockManager blockManager) { this.blockManager = blockManager; }
+
+			public int this[int x, int z] {
+				[MethodImpl(MethodImplOptions.AggressiveInlining)]
+				get { return blockManager.GetChunk(x, z).HeightMap[x & Chunk.XMask, z & Chunk.ZMask]; }
+			}
+
+		}
+
+		private HeightMapIndexer heightMapIndexer;
+		public HeightMapIndexer HeightMap { get { return heightMapIndexer ?? (heightMapIndexer = new HeightMapIndexer(this)); } }
 	}
 }
